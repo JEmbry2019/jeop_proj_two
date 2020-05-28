bf676a4bea2bc3973687f49c85e2bf45033af564
JEmbry2019 <jmsembry@gmail.com>
2020-05-28 13:48:51 -0400

    code update

--- jeop_game.py
+++ jeop_game.py
@@ -2,11 +2,21 @@ import csv
 with open('jeop_quest.csv', newline='') as csvfile:
  quest = csv.DictReader(csvfile)
  rows = list(quest)
- for row in rows[2:4]:
+ for row in rows[0:4]:
     print(row)
 
-    
 
+
+# This code gives a KEY error
+
+# with open('jeop_quest.csv', newline='') as csvfile:
+#  quest = csv.DictReader(csvfile)
+#  rows = list(quest)
+#  for row in rows[0:4]:
+#     print(row['Show Number'], row['Air  Date'], row['Round'])
+
+    
+ # This code works
 # data = csv.DictReader(open("jeop_quest.csv"))
 # print("CSV file as a dictionary ROW-------------------------------------------------------------------------------:\n")
 # for row in data:
